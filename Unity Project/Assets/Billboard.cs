bab32131eea571a0b0684605b4ab3dc35cce729e
DESKTOP-RCS8KTK\cflorea <Ciprian.Florea@oulu.fi>
2019-02-18 16:06:20 +0200

    Billboard tree

---
+++ Unity Project/Assets/Billboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+
+    public float damping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform target = GameObject.Find("Main Camera").transform;
+        var lookPos = target.position - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+    }
+}
