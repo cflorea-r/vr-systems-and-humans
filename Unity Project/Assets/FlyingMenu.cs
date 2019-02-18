07259eda4a7daf4b255c40cc89e8397c921cd615
DESKTOP-RCS8KTK\cflorea <Ciprian.Florea@oulu.fi>
2019-02-18 11:07:58 +0200

    fly

--- Unity Project/Assets/FlyingMenu.cs
+++ Unity Project/Assets/FlyingMenu.cs
@@ -5,6 +5,9 @@ using DG.Tweening;
 
 public class FlyingMenu : MonoBehaviour
 {
+
+    public bool fly = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +17,16 @@ public class FlyingMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (fly)
+        {
+            Fly();
+            fly = false;
+        }
     }
 
     public void Fly()
     {
-
+        GameObject gameObject = GameObject.Find("Canvas");
+        gameObject.transform.DOMove(Vector3.zero, 1, false);
     }
 }
