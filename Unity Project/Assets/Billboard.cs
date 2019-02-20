712860e21436ad2840337f83f46450c1defe55cf
DESKTOP-RCS8KTK\cflorea <Ciprian.Florea@oulu.fi>
2019-02-20 09:30:55 +0200

    Billboard

--- Unity Project/Assets/Billboard.cs
+++ Unity Project/Assets/Billboard.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
 
-    public float damping;
+    [Range(0, 5)]
+    public int damping;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +17,14 @@ public class Billboard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Transform target = GameObject.Find("Main Camera").transform;
-        var lookPos = target.position - transform.position;
-        lookPos.y = 0;
-        var rotation = Quaternion.LookRotation(lookPos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+        foreach (Transform tree in transform)
+        {
+            //Transform target = GameObject.Find("Main Camera").transform;
+            Transform target = UnityEditor.SceneView.lastActiveSceneView.camera.transform;
+            var lookPos = target.position - tree.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            tree.rotation = Quaternion.Slerp(tree.rotation, rotation, Time.deltaTime * damping);
+        }
     }
 }
