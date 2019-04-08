30059cedd7b3addd9ef20ca6254fda60d56f4fd1
flciprian <flciprian262@gmail.com>
2019-04-08 13:23:48 +0300

    Implemented basic functionality, no fancy surroundings

--- Unity Project VR Samples/Assets/Fear of heights/Fearofheights.cs
+++ Unity Project VR Samples/Assets/Fear of heights/Fearofheights.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Fearofheights : MonoBehaviour
 {
+
+    public float endValue;
+    public float duration;
+    public float delay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject player = GameObject.Find("Player");
+        player.transform.DOMoveY(endValue, duration, false).SetDelay(delay);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
