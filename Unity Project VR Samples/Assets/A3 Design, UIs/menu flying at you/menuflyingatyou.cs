1437f1105d8b0c1193051b94cbfa9888debf5d89
flciprian <flciprian262@gmail.com>
2019-04-08 14:04:53 +0300

    menu flying at you

---
+++ Unity Project VR Samples/Assets/A3 Design, UIs/menu flying at you/menuflyingatyou.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class menuflyingatyou : MonoBehaviour
+{
+
+    public float endValue;
+    public float duration;
+    public float delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+        canvas.transform.position = new Vector3(0, 0, 100);
+        canvas.transform.DOMoveZ(endValue, duration, false).SetDelay(delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
