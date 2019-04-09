84407baff7fd9f62e279f122ef4c97c3c7ad808b
flciprian <flciprian262@gmail.com>
2019-04-09 12:41:38 +0300

    added modes to scene 6

--- Unity Project VR Samples/Assets/MenuthatisaslargeasFOV.cs
+++ Unity Project VR Samples/Assets/MenuthatisaslargeasFOV.cs
@@ -10,6 +10,8 @@ public class MenuthatisaslargeasFOV : MonoBehaviour
     // Adjust the speed for the application.
     public float speed = 1.0f;
 
+    public int mode = 3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,21 +25,45 @@ public class MenuthatisaslargeasFOV : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetButtonDown("Fire1"))
         {
-            canvas.SetActive(true);
-            canvas2.SetActive(false);
+            if (mode == 3)
+            {
+                mode = 1;
+                print("mode 1");
+                canvas.SetActive(true);
+                canvas2.SetActive(false);
+            }
+            else if (mode == 1)
+            {
+                mode = 2;
+                print("mode 2");
+                canvas2.SetActive(true);
+                canvas.SetActive(false);
+            }
+            else if (mode == 2)
+            {
+                mode = 3;
+                print("mode 3");
+                canvas.SetActive(false);
+                canvas2.SetActive(false);
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            canvas2.SetActive(true);
-            canvas.SetActive(false);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            canvas.SetActive(false);
-            canvas2.SetActive(false);
-        }
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    canvas.SetActive(true);
+        //    canvas2.SetActive(false);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+        //    canvas2.SetActive(true);
+        //    canvas.SetActive(false);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.Alpha3))
+        //{
+        //    canvas.SetActive(false);
+        //    canvas2.SetActive(false);
+        //}
 
         // Move our position a step closer to the target.
         float step = speed * Time.deltaTime; // calculate distance to move
